b5ff2f084fade45a1cb463ee159e0bd4eefa464f
Joe Pearson <joe.pearson@avid.com>
2020-08-28 14:53:38 -0700

    add app

---
+++ src/components/app.tsx
@@ -0,0 +1,13 @@
+import { FunctionalComponent, h } from "preact";
+
+import { Modal } from "react-bootstrap";
+
+const App: FunctionalComponent = () => {
+    return (
+        <div id="app">
+            <Modal.Dialog></Modal.Dialog>
+        </div>
+    );
+};
+
+export default App;
